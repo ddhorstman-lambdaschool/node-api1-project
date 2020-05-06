1e557941ddc33815f22fab0df1b25ec9ccd4ec28
David Horstman <davidzilla27@gmail.com>
2020-05-06 15:07:43 -0400

    Cleanup

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const fs = require("fs");
 const server = express();
 
 const PORT = process.env.PORT || 5000;
+//import user data from saved file on startup
 let users = JSON.parse(fs.readFileSync("./data/users.json"));
 
 server.use(express.json());
 
+//save updated date to file
 function saveUsers(res) {
   try {
     fs.writeFileSync("./data/users.json", JSON.stringify(users, null, 2));
+    //return code 0 - no error
     return 0;
   } catch {
     res
@@ -20,6 +23,7 @@ function saveUsers(res) {
         errorMessage: "There was an error while updating the database.",
       })
       .end();
+    //return code 1 - some error
     return 1;
   }
 }
@@ -39,6 +43,7 @@ server.get("/api/users/:id", (req, res) => {
   try {
     const { id } = req.params;
     const found = users.find(user => user.id == id);
+
     found
       ? res.status(200).json(found)
       : res.status(404).json({
@@ -57,22 +62,21 @@ server.get("/api/users/:id", (req, res) => {
 server.post("/api/users", (req, res) => {
   const { name, bio } = req.body;
   if (!(name && bio)) {
-    res.status(400).json({
+    return res.status(400).json({
       errorMessage:
         "Please provide both a 'name' and a 'bio' field for the user.",
     });
-  } else {
-    const entry = { id: shortID.generate(), name, bio };
-    try {
-      users.push(entry);
-      //return value of 0 means the operation succeeded
-      saveUsers(res) === 0 && res.status(201).json(entry);
-    } catch {
-      res.status(500).json({
-        errorMessage:
-          "There was an error while saving the user to the database.",
-      });
-    }
+  }
+
+  const entry = { ...req.body, id: shortID.generate() };
+  try {
+    users.push(entry);
+    //return value of 0 means the operation succeeded
+    saveUsers(res) === 0 && res.status(201).json(entry);
+  } catch {
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database.",
+    });
   }
 });
 
@@ -82,9 +86,9 @@ server.post("/api/users", (req, res) => {
 server.patch("/api/users/:id", (req, res) => {
   try {
     const { id } = req.params;
-    let found = users.find(user => user.id == id);
+    const found = users.find(user => user.id == id);
     if (found) {
-      Object.assign(found, req.body);
+      Object.assign(found, { ...req.body, id });
       //return value of 0 means the operation succeeded
       saveUsers(res) === 0 && res.status(200).json(found);
     } else {
@@ -106,24 +110,24 @@ server.put("/api/users/:id", (req, res) => {
   try {
     const { name, bio } = req.body;
     const { id } = req.params;
-    let foundIdx = users.findIndex(user => user.id == id);
-    if (foundIdx !== -1) {
-      if (!(name && bio)) {
-        res.status(400).json({
-          errorMessage:
-            "Please provide both a 'name' and a 'bio' field for the user.",
-        });
-      } else {
-        const entry = { ...req.body, id };
-        users[foundIdx] = entry;
-        //return value of 0 means the operation succeeded
-        saveUsers(res) === 0 && res.status(200).json(entry);
-      }
-    } else {
-      res.status(404).json({
+    const foundIdx = users.findIndex(user => user.id == id);
+    if (foundIdx === -1) {
+      return res.status(404).json({
         message: `No user found with id '${id}'.`,
       });
     }
+
+    if (!(name && bio)) {
+      return res.status(400).json({
+        errorMessage:
+          "Please provide both a 'name' and a 'bio' field for the user.",
+      });
+    }
+
+    const entry = { ...req.body, id };
+    users[foundIdx] = entry;
+    //return value of 0 means the operation succeeded
+    saveUsers(res) === 0 && res.status(200).json(entry);
   } catch {
     res.status(500).json({
       errorMessage: "There was an error while updating the user.",
@@ -137,7 +141,7 @@ server.put("/api/users/:id", (req, res) => {
 server.delete("/api/users/:id", (req, res) => {
   try {
     const { id } = req.params;
-    let found = users.find(user => user.id == id);
+    const found = users.find(user => user.id == id);
     if (found) {
       users = users.filter(user => user.id != id);
       //return value of 0 means the operation succeeded
